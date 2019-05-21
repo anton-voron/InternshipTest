95ed03f89ee042c04196a293b312c7c76326b398
anton-voron <anton.voron.gt@gmail.com>
2019-05-21 23:14:44 +0300

    fix

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
 var input1 = document.getElementById('input1');
-input1.validate = false;
-input1.src;
-input1.e;
-
 var input2 = document.getElementById('input2');
-input2.validate = false;
-input2.src;
-input2.e;
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
  
-
-
 input1.addEventListener('change', firstHandleImage);
 input2.addEventListener('change', secoundHandleImage);
 
 function upload (img, x, y, x2, y2) {
-	
-		if (flipper.name == "row") {
-			var width = (img.width * 100 / img.height);
-			ctx.clearRect(x, y, 200, 100);
-			ctx.clearRect(x2, y2, 200, 100);
-			ctx.drawImage(img, x, y, width, 100);
-		} else if (flipper.name == "column") {
-			var width = (img.width * 100 / img.height);
-			ctx.clearRect(x, y, 200, 100);
-			ctx.clearRect(x2, y2, 200, 100);
-			ctx.drawImage(img, x2, y2, width, 100);
-		}
-	
+	var width = (img.width * 100 / img.height);
+	ctx.clearRect(x, y, 200, 100);
+	ctx.clearRect(x2, y2, 200, 100);
+	if (flipper.name == "row") {
+		ctx.drawImage(img, x, y, width, 100);
+	} else if (flipper.name == "column") {
+		ctx.drawImage(img, x2, y2, width, 100);
+	}
 };
 
 function uploadReader (e, input, x, y, x2, y2) {
@@ -45,58 +31,37 @@ function uploadReader (e, input, x, y, x2, y2) {
 	input.e = e;
 };
 
+function uploadValidator () {
+	if (input1.value) {
+			uploadReader (input1.e, input1, 0, 0, 0, 0);
+	}; 
+	if (input2.value) {
+		uploadReader (input2.e, input2, 230, 0, 0, 120);
+	}
+};
+
 
 function firstHandleImage (e) {
 		e.preventDefault();
-		input1.validate = true;
-		uploadReader (e, input1, 0, 0, 0, 0);
-		
-		uploadReader (input2.e, input2, 230, 0, 0, 120);
+		uploadReader (e, input1, 0, 0, 0, 0);	
 };
 
 function secoundHandleImage (e) {
 	e.preventDefault();
-	input2.validate = true;
-		uploadReader(e, input2, 230, 0, 0, 120);
-		
+		uploadReader(e, input2, 230, 0, 0, 120);		
 };
 
-var flipper = document.querySelector('.switch-btn');
+var flipper = document.getElementById('checkbox');
 flipper.addEventListener('click', update);
-
-var circle = document.querySelector('.btn-indicator');
-
-function update (evt) {
-	evt.preventDefault();
+function update () {
 	if (flipper.name == "row") {
 		flipper.name = "column";
-		if( input1.validate == true && input2.validate == true) {
-			uploadReader (input1.e, input1, 0, 0, 0, 0);
-			uploadReader (input2.e, input2, 230, 0, 0, 120);
-		}
-	  
-		flipper.classList.remove("switch-btn-disable");
-		flipper.classList.add("switch-btn-active");
-
-		circle.classList.remove("disable");
-		circle.classList.add("active");
-	} 
+		uploadValidator();
+		flipper.checked = true;	
+	}
 	else if(flipper.name == "column") {
 		flipper.name = "row";
-		if( input1.validate == true && input2.validate == true) {
-			uploadReader (input1.e, input1, 0, 0, 0, 0);
-			uploadReader (input2.e, input2, 230, 0, 0, 120);
-		}
-		
-		flipper.classList.remove("switch-btn-active");
-		flipper.classList.add("switch-btn-disable");
-
-		circle.classList.remove("active");
-		circle.classList.add("disable");
-
+		uploadValidator();
+		flipper.checked = false;
 	}
 }
-
-
-
-					
